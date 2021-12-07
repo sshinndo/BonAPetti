29e878f980e0f4a2433f4ed5fc5cae57b550acab
Shin Do Hyun <sdh0729@naver.com>
2021-12-07 18:21:20 +0900

    Delay_screen 로고 위치 중앙으로 변경

--- lib/log_in/screen/delay_screen.dart
+++ lib/log_in/screen/delay_screen.dart
@@ -4,11 +4,11 @@ import 'dart:async';
 import 'package:pet_service_application/log_in/class/UserData.dart';
 import 'package:pet_service_application/main.dart';
 
-
 class DelayScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height / 800;
+
     //데이터 로딩
     UserData.getUserData(Logger().userData.uid.toString()).then((updateLogger) {
       Logger().userData = updateLogger;
@@ -16,33 +16,22 @@ class DelayScreen extends StatelessWidget {
     });
     Timer(
         Duration(seconds: 7),
-            () =>
-            Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (BuildContext context) => MyHomePage())));
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => MyHomePage())));
 
     return Scaffold(
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                child: Image.asset('images/logo.png', alignment: Alignment.center),
-                width: 192,
-                height: 192,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircularProgressIndicator(),
-            ],
-          )
-        ]
-      ),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+        SizedBox(height: height * 300),
+        Container(
+          alignment: Alignment.center,
+          child: Column(children: [
+            Image.asset('images/logo.png', width: 192, height: 192),
+            CircularProgressIndicator()
+          ]),
+        ),
+      ]),
     );
   }
 }
